838150c3c1b44de2589193d38d37a34a778939f1
Enzo Godoy <daios.inc@gmail.com>
2020-10-06 18:13:57 +0200

    Added JQuery traversing nodes notes

--- Jquery/jquery_02.js
+++ Jquery/jquery_02.js
@@ -356,9 +356,148 @@ $("button").click(function(){
 	$("#div1").width(500).height(500);
 }); 
 
+/* 
+Traversing : Means "move through", it's a concept involving hoe to move from different nodes in  a set of nodes
+Select some of them, avoid others by using properties from the node selected. 
+
+Sample in html the most usual relation also used in CSS are : 
+parent, ancestor, child, sibling, descendant. 
+
+Through this we make a "concept" of three with all their nodes being represantation of these nodes
+(parent, ancestor, child, sibling, descendante) tracing a route from one node to the other and so on. 
+
+For this in JQuery the there are some methods calls tree-traversal. 
+
+Also in JQuery, Xpath, XML, HTML, Json the same concept it's used, also on groovy for make a selection of specific
+node in unit test by some tools. 
+
+Some function to traverse the DOM tree 
+parent() 
+parents() 
+parentsUntil() 
+
+parent >> Retrieve just the direct parent
+parents >> Retrieves all the parents from the element selected to the root (html)
+parentsUntil >> Can retrieve all the parents up to an element 
+
+Descendant it´s a representation for (children, grandchild, grandchild, great-granchild, etc)
+
+children() >> Returns all direct children of the selected element 
+find() >> Return all the descendant element from the selected 
+
+*/ 
+
+-- Sample returning the direct parent from a "span" element 
+$(document).ready(function(){ 
+	$("span").parent();
+}); 
+
+-- Sample returning all the parents up to the root (html) from a "span" element 
+$(document).ready(function(){ 
+	$("span").parents();
+}); 
+
+
+-- Sample returning 'ul' elements parents from an "span" element 
+$(document).ready(function(){ 
+	$("span").parents("ul");
+}); 
+
+-- Sample returning all the parents up to  the first "div" element 
+$(document).ready(function(){ 
+	$("span").parentsUntil("div");
+});
+
+
+-- Sample returning all the children element from a "div" 
+$(document).ready(function(){
+	$("div").children();
+}); 
+
+//Interesting we can add special selector to the function children() as a filter 
+-- Sample returning element children from a filtered element 
+$(document).(function(){ 
+	$("div").children("p.someSelector"); 
+}); 
+
+//Find support special characters also 
+-- Sample of retrieving all the element of type "span" from a div 
+$(document).(function(){ 
+	$("div").children("span");
+}); 
+
+-- Sample of retrieving all the descendent from  a div 
+$(document).ready(function(){ 
+	$("div").find("*");
+}); 
+
+/* 
+Some useful function to travers sideways the dom tree 
+	siblings() 
+	next() 
+	nextAll() 
+	nextUntil() 
+	prev() 
+	prevAll() 
+	prevUntil() 
+
+next(), nextAll(), nextUntil() >> Retrieves going forward 
+prev(), prevAll(), prevUntil()  >> Retrieves going backwards
+
+Some others methos to filtrate 
+first(), last(), eq() :  
+Allow us to select a specific element based on its position in a group of elements 
+
+ filter() and not() :
+Allow us to select elements by filtering by certain criteria. 
+
+*/ 
+
+-- Sample of retrieving all the siblings from an element 
+$(document).ready(function(){ 
+	$("div").siblings();
+}); 
+
+-- Sample of retrieving all the siblings with identified "p" from a div
+$(document).ready(function(){ 
+	$("div").siblings("p"); 
+}); 
+
+-- Sample returning all the siblings elements from an "h2" element 
+$(document).ready(function(){
+	$("h2").nextAll();
+}); 
+
+
+-- Sample returining element up to <h6> element 
+$(document).ready(function(){ 
+	$("h2").nextUntil("h6");
+}); 
 
 
 
+-- Sample of retrieving the first element
+$(document).ready(function(){ 
+	$("div").first();
+}); 
 
+-- Sample of retrieving the last element 
+$(document).ready(function(){ 
+	$("div").last();
+});
 
+-- Sample of retrieves with same index in element tracing 
+$(document).ready(function(){ 
+	$("p").eq(1);
+}); 
+
+-- Sample of retrieving with a class "intro" element 
+$(document).ready(function(){ 
+	$("p").filter(".intro");
+});
+
+-- Sample of retrieving elements those whose doesn´t have "intro" class 
+$(document).ready(function(){ 
+	$("p").not(".intro");
+}); 
 
