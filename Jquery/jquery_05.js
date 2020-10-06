9b97eb9a42bb388ef68775893306a50b8e4ccdfd
Enzo Godoy <daios.inc@gmail.com>
2020-10-06 19:46:04 +0200

    Added JQuey filter|release notes

--- Jquery/jquery_05.js
+++ Jquery/jquery_05.js
@@ -174,3 +174,52 @@ $("button").click(function(){
 }); 
 
 
+/* 
+If two JS Frameworks use the same symbol as a shortcut one of them might stop working. 
+To avoid this the JQuery team develop the (noConflict()) function to avoid collition of symbols and release it 
+
+Sample in JQuery using '$' as a shortcut for JQuery.. 
+Also by saving the release value return we can implement a shortcut of our own 
+ var jq = $.noConflict();
+
+Also we can block the symbol within a method ready() some related to scope thing. And making it 
+blocked for used of jQuery only within the function
+
+*/ 
+
+-- Sample with release of '$' with noConflict() 
+$.noConflict(); 
+	jQuery(document).ready(function(){ 
+		jQuery("button").click(function(){
+			jQuery("p").text("jQuery is still working!");
+		}); 
+	}); 
+
+-- Sample with relasing the symbol '$' and replacing it 
+var jq = $.noConflict(); 
+ jq(document).ready(function(){ 
+ 	jq("button").click(function(){ 
+ 		jq("p").text("jQuery is still working!");
+ 	});
+ }); 
+
+-- Sample with the symbol being blocked 
+$.noConflict(); 
+ jQuery(document).ready(function($){
+ 	$("button").click(function(){
+ 		$("p").text("jQuery still in here!");
+ 	});
+ }); 
+
+
+-- Sample implementing filtering on a table with id "myTable" 
+<script> 
+$(document).ready(function(){ 
+	$("#myInput").on("keyup", function(){ 
+		var value = $(this).val().toLowerCase(); 
+		$("#myTable tr").filter(function() {
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -! );
+		});
+	}); 
+}); 
+
