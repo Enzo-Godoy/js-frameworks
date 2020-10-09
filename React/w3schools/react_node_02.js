e2828c77f6abefafa3dcc1542d8b1522fd30e9b0
Enzo Godoy <daios.inc@gmail.com>
2020-10-09 19:44:37 +0200

    Added info of react components

--- React/w3schools/react_node_02.js
+++ React/w3schools/react_node_02.js
@@ -128,29 +128,71 @@ ReactDOM.render(myelement, document.getElementById('root'));
 ReactDOM.render(<p>Hello</p>, document.getElementByID('sandy'); 
 
 
+/* 
+JSX >> It's code from a mixing of JS and HTML that it's transpiled to HTML by babel and doesn't required any 
+	   appending method to HTML elements. 
+	   
+	   for this elements declared in variables that represent chuncks of code are used and passes as reference to the 
+	   ReactDOM.render method. 
+	   With JSX we can write expressions inside curly braces {} 
+	   The expression can be a React variable, or property, or any other JS valid expression. Interpreted at runtime.
+	   
+	   If we want to write multiple tag elements in a component we should wrapp them in parenthesis, also 
+	   Always we need to remember to make an only parent 'HTML Element' of the new component. Also remember always to close the elements!.
+	   
+	   React Components : Are independent and reusable bits of code. They serve the same purpose as JavaScript functions, 
+	   but work in isolation and returns HTML via a render function.
+	   
+	   Two types : 
+	   * Class Components. 
+	   * Function Components. 
+	   
+	   Class Components : The component hast to include the extends React.Component statement, this statement creates an inheritance to
+						  React.Compoennts, and gives our component acces to React.Component's functions. 
+						  Also rquired the render() method.
+		Function Components : Behaves almost similar to the Class component, but class component posses other feature that makes it particular. 
+	
+
+If there is a constructor() function in our component, this function will be called when the component gets initiated.  
+		constructor is where we initiate the component's properties. 
+		
+		In React, compoennt properties should be kept in an object called state. 
+		In the constructor function we could use the super() statement that gives us acces to all the function of the parent
+		component (React.Component) due to a the 'extends React.Component' propertie of the class. 
+		
+		We could change properties of the component by changing the attributes of the component elements. This way thanks 
+		to the 'this' reference the value from the element are updated. 
+		
+		Usually compoennt are keep in separate files in order to modulate them. 
+			After being divided we should remember to include them by exporting them as resource at the top of our 'Render() file'
+*/ 
+
+-- Sample JSX 1 
+const myelement  = <h1> Random Text '1</h1>; 
+ReactDOM.render(myelement, document.getElementById('root')); 
+
+-- Sample without JSX 
+const myelement = React.createElement('h1', {}, 'Some random coden 02'); 
+ReactDOM.render(myelement, document.getElementById('root')); 
+
+-- Sample of a expression in curly races 
+const myelement = <h1>React some { 0 + 1 } result </h1> 
+
+-- Sample of a component multiline 
+const myelement = (
+	<ul> 
+	 <li>Apples</li> 
+	 <li>Bananas</li> 
+	 <li>Cherries</li> 
+	</ul>
+);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- Sample of a parent elemnt for a component 
+const myelement = ( 
+	<div> 
+		<h1>Some example</h1>
+		<h1>Some example 02</h1>
+	</div> 
+);
 
