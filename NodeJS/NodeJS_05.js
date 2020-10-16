f246ec8026bb8e137a115d28c9b2c19b3461fe4b
Enzo Godoy <daios.inc@gmail.com>
2020-10-16 17:59:47 +0200

    Added notes and Creation DB, Collection in MongoDB

--- NodeJS/NodeJS_05.js
+++ NodeJS/NodeJS_05.js
@@ -148,15 +148,54 @@ con.connect(function (err) {
 	});
 });
 
+/* 
+One of  the most popular NoSQL database is MongoDB.  To install it 
+ ´´
+	npm install mongodb 
+ ´´ 
+ 
+ And for making it available in our code 
+ var mongo = require('mongodb'); 
+ To create a database in MongoDB, we start by creating a MongoClient object, the specify a connection URL with the correct ip address 
+ and the name of the databse we want to create. MDB will create the database if it does not exist, and make a connection to it. 
+
+ Note : In MDB,  a database is not created until it gets content!. 
+ MongoDB waits util we have created a collection(table), with at least one document (record) before it actually creates the database (and collection). 
+*/ 
+
+-- Sample of creating a connection to a mongodb db 
+var MongoClient = require('mongodb').MongoClient; 
+var url = "mongodb://localhost:27017/mydb"; 
+
+MongoClient.connect(url, function(err, db) {
+	if(err) throw err;
+	console.log("Database Created!"); 
+	db.close();
+});
 
 
+-- Sample of creating a collection using a MongoClient from the module 'mongodb' 
+var MongoClient = require('mongodb').MongoClient; 
+var url = "mongodb://localhost:27017/"; 
 
+MongoClient.connect(url, function(err, db) {
+	if(err) throw err; 
+	var dbo = db.db("mydb"); 
+	dbo.createCollection("customers", function(err, res) {
+		if(err) throw err; 
+		console.log("Collection Created!"); 
+		db.close();
+	});
+});
 
-
-
-
-
-
-
-
-
+/* 
+To insert a record, or  document as it called in MongoDB, into a collection, we use the insertOne(9 method. 
+ document(MongoDB) <<< Same >> Record(MySQL) 
+ 
+ The first parameter of the insertOne() method is an object containing the name, and value of each field in the document we want to insert. 
+ In order to insert multiple documents into a colletion in MDB, we use the insertMany() method. 
+	The first parameter of the insertMany() method is an array of objects, containing the data we want to insert. 
+	
+	When executing the insertMany() method, a result object is returned. This object contains information abouthow the insertien affected the DB. 
+	If we do not specify an _id field, then MongoDB will add one for us and assign a unique id for each document. 
+*/
