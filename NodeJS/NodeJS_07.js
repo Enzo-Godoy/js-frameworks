d1a83ee2de0f1232bf027b038696e00c7d0da4a7
Enzo Godoy <daios.inc@gmail.com>
2020-10-22 18:29:47 +0200

    Added notes and examples on update Records

--- NodeJS/NodeJS_07.js
+++ NodeJS/NodeJS_07.js
@@ -115,133 +115,84 @@ MongoClient.connect(url, function(err, db) {
 	});
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* 
+Drop Collection >> We use the drop method in order to deleter a collection(table) in mongo db. It takes a callback function containing the error object
+and the result parameer wich returns true if the collection was dropped successfully, otherwise it returns false. 
+
+We can use 'db.dropCollection' 'db.collection().drop()' to achieve the same purpose, delete a collection. 
+
+Update records >> To perform an update of a record, or  document by using the 'updateOne()' method. If more than one record is found only the first one ends updated. 
+The second parameter is an object defining the nre values of the document. 
+
+ When using the $set operator, only the specified field are updated. 
+ In order to update multiple records the 'updateMany()' method is used.
+*/ 
+
+-- Sample of dropping the 'customers' collection by using 'drop' method.
+var MongoClient = require('mongodb').MongoClient; 
+var url = "mongodb://localhost:27017/"; 
+
+MongoClient.connect(url, function(err, db) {
+	if(err) throw err; 
+	var dbo = db.db("mydb"); 
+	dbo.collection("customers").drop(function (err, delOK) {
+		if(err) throw err; 
+		if(delOK) console.log("Collection deleted"); 
+		db.close();
+	});
+});
+
+-- Sample of dropping the 'customers' collection by using 'dropCollection' method. 
+var MongoClient = require('mongodb').MongoClient; 
+var url = "mongodb://localhost:27017/"; 
+
+MongoClient.connect(url, function(err, db) {
+	ir(err) throw err; 
+	var dbo = db.db("mydb"); 
+	dbo.dropCollection("customers", function(err, delOK) {
+		if(err) throw err; 
+		if(delOK) console.log("Collection deleted");
+		db.close();
+	});
+});
+
+-- Sample  of updating a record by using updateOne method 
+var MongoCleint = require('mongodb').MongoClient; 
+var url = "mongodb://localhost:27017/"; 
+
+MongoClient.connect(url, function(err, db) {
+	if(err) throw err; 
+	var dbo = db.db("mydb"); 
+	var myquery = {	address : "Valley 345" };
+	var newvalues = { $set: { name: "Mickey", address : "Canyon 123" } }; 
+	dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+		if(err) throw err; 
+		console.log("1 document updated"); 
+		db.close();
+	});
+});
+
+
+-- Sample of using the $set operator 
+.. 
+ var myqyery = { address : "valley 345"};
+ var newvalues = { $set : { address : "Canyon 123" } } ;
+ dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {}
+.. 
+
+
+-- Sample of using a regex expression to update multiple records and the "updateMany" method 
+var MongoClient = require('mongodb').MongoClient; 
+var url = "mongodb://localhost:27017/"; 
+
+MongoClient.connect(url, function(err, db) {
+	if(err) throw err; 
+	var dbo = db.db("mydb"); 
+	var myquery = { address : /^S/ }; 
+	var newvalues = { $set: {name: "Minnie"} }; 
+	dbo.collection("customers").updateMany(myquery, newvalues,function(err, res) {
+		if(err) throw err; 
+		console.log(res.result.nModified + "document(s) updated");
+		db.close();
+	});
+});
